7821dae409b4ce1f173eb3edd788891a17821e8a
Harinisreesampath <30902956+Harinisreesampath@users.noreply.github.com>
2021-04-02 12:14:45 +0530

    Add files via upload
    Build error corrections

---
+++ colorcoder/Main.java
@@ -0,0 +1,13 @@
+package colorcoder;
+
+public class Main {    
+
+    public static void main(String[] args) {
+    	ReferenceManual.printReferenceManual();
+    	ColorPairTest.testNumberToPair(4, MajorColor.WHITE, MinorColor.BROWN);
+    	ColorPairTest.testNumberToPair(5, MajorColor.WHITE, MinorColor.SLATE);
+    
+    	ColorPairTest.testPairToNumber(MajorColor.BLACK, MinorColor.ORANGE, 12);
+    	ColorPairTest.testPairToNumber(MajorColor.VIOLET, MinorColor.SLATE, 25);
+    }
+}
